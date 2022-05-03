ef90cb4503f1759a23054aeddf1d4b11e645d7f9
PhillipT13 <phillipthen@gmail.com>
2022-05-03 23:07:21 +1200

    Initial Commit

---
+++ Pong/src/main/Pong.java
@@ -0,0 +1,31 @@
+package main;
+
+
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+
+public class Pong extends JFrame{
+
+	public Pong() {
+    	setTitle("Pong");
+    	setSize(800, 600);
+    	setResizable(false);
+    	add(new PongPanel());
+    	//Frame jframe = null;
+    	JFrame frame = new JFrame();
+    	frame.setBackground(Color.BLACK);
+    	setVisible(true);
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	       public void run() {
+	    	   new Pong();
+           }
+       });
+	}
+}
---
+++ Pong/src/main/PongPanel.java
@@ -0,0 +1,74 @@
+package main;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener{
+
+	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	 private final static int TIMER_DELAY = 5;
+	      
+	  public PongPanel() {
+	      setBackground(BACKGROUND_COLOUR);
+	      Timer timer = new Timer(TIMER_DELAY, this);
+	         timer.start();
+	 }
+	  
+	  private void paintDottedLine(Graphics g) {
+	      Graphics2D g2d = (Graphics2D) g.create();
+	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	         g2d.setStroke(dashed);
+	         g2d.setPaint(Color.WHITE);
+	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	         g2d.dispose();
+	 }
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	 public void paintComponent(Graphics g) {
+	     super.paintComponent(g);
+	     g.setColor(Color.WHITE);
+	     g.fillRect(20, 20, 100, 100);
+	     paintDottedLine(g);
+	 }
+	
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		//add setBackground(Color.BLACK)
+		//JFrame window = new JFrame();
+		//window.getContentPane.setBackground(Color.RED);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	private void update() {
+        
+	 }
+	  
+	 @Override
+	 public void actionPerformed(ActionEvent event) {
+	      update();
+	      repaint();
+	 }
+}
